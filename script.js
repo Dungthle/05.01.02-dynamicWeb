92941efe8547007fb7132769fbb673a593b31179
Dung Le <DungLe@laptop-8nugmqjn.kea.local>
2020-02-12 12:59:17 +0100

    AddDiscountPrice

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ function showSingleDish(dish){
 
     clone.querySelector("h2").textContent = dish.name;
     clone.querySelector("h3").textContent = dish.shortdescription;
-    clone.querySelector("p").textContent = dish.price;
+    clone.querySelector("p.price-full span").textContent = dish.price;
+    clone.querySelector("p.price-discount span").textContent = dish.discount;
 
 
     const imageName = dish.image; // this would be dynamic
@@ -29,6 +30,18 @@ function showSingleDish(dish){
     const mediumImg = base + "medium/" + imageName + "-md.jpg";
     const largeImg = base + "large/" + imageName + ".jpg";
     clone.querySelector("img").src = smallImg;
+
+    if (dish.discount) {
+        console.log("yeah")
+        clone.querySelector(".price-full span").textContent=dish.price;
+        //calculate new price
+        const newPrice = Math.round(dish.price - dish.price * dish.discount / 100);
+        clone.querySelector(".price-discount span").textContent=newPrice;
+
+    } else {
+        clone.querySelector(".price-discount").remove()
+        clone.querySelector(".price-full span").textContent=dish.price;
+    }
 
     const parent = document.querySelector("main");
     parent.appendChild(clone)
